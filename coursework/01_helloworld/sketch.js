690d0b2b7362963b9bd93f88e8f45f52cef60ad1
Daylin Scott <scottdaylin@gmail.com>
2018-09-12 19:16:04 -0600

    adding hw p5 examples and committing homework 3

---
+++ coursework/01_helloworld/sketch.js
@@ -0,0 +1,3 @@
+function setup(){
+    text("Hello Daylin!", 0, 10);
+}
---
+++ hw-3/sketch.js
@@ -0,0 +1,17 @@
+// I am declaring the function setup
+function setup() {
+
+  // Creating 600x400 canvas
+  createCanvas ( 600, 600 );
+
+  //Color background pink
+  background( 'yellow' );
+}
+
+
+// Declare draw function
+function draw (){
+
+  // With the text function have p5 write hello world
+  text( "Hello, World!", 300, 300 );
+}
